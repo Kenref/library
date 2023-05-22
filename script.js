91cb14662ef605853505e91da8cb9fa0540b80a5
Kenref <107287396+Kenref@users.noreply.github.com>
2023-05-22 16:07:53 +1000

    Format the document

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const modal = document.getElementById("modal");
 const openModal = document.getElementById("open-modal-button");
 const closeModal = document.getElementById("close-modal-button");
-const submitBookButton = document.getElementById("submit-book-button")
-let booksContainer = document.querySelector(".books-container")
-const removeButtons = document.querySelectorAll(".remove-button")
-const documentBody = document.querySelector("body")
+const submitBookButton = document.getElementById("submit-book-button");
+let booksContainer = document.querySelector(".books-container");
+const removeButtons = document.querySelectorAll(".remove-button");
+const documentBody = document.querySelector("body");
 
-const title = document.getElementById("title")
-const author = document.getElementById("author")
-const pages = document.getElementById("pages")
-const readingStatus = document.querySelectorAll("input[name='status']")
-
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const readingStatus = document.querySelectorAll("input[name='status']");
 
 openModal.addEventListener("click", () => {
 	modal.showModal();
@@ -20,47 +19,37 @@ closeModal.addEventListener("click", () => {
 	modal.close();
 });
 
-
 //removing books
 booksContainer.addEventListener("click", (e) => {
 	if (e.target.classList.contains("remove-button")) {
 		const bookCard = e.target.parentNode;
-		const index = Array.from(booksContainer.children).indexOf(bookCard)
-		library.splice(index, 1)
-		bookCard.remove()
+		const index = Array.from(booksContainer.children).indexOf(bookCard);
+		library.splice(index, 1);
+		bookCard.remove();
 	}
-})
+});
 
 //toggle read
 booksContainer.addEventListener("click", (e) => {
 	if (e.target.classList.contains("toggle-read")) {
-		let status = e.target.parentNode.children[3]
+		let status = e.target.parentNode.children[3];
 		if (status.innerHTML == "Unread") {
-			status.innerHTML = "Read"
-		}
-		else {
-			status.innerHTML = "Unread"
+			status.innerHTML = "Read";
+		} else {
+			status.innerHTML = "Unread";
 		}
 	}
-})
-
-
-
+});
 
 function getStatus(statuss) {
 	for (let i = 0; i < statuss.length; i++) {
 		if (statuss[i].checked) {
 			return "Read";
-		}
-		else {
-			return "Unread"
+		} else {
+			return "Unread";
 		}
 	}
 }
-
-
-
-
 
 let library = [];
 
@@ -71,7 +60,6 @@ function Book(title, author, pages, status) {
 	this.status = status;
 }
 
-
 function addBookToLibrary(title, author, pages, status) {
 	library.push(new Book(title, author, pages, status));
 }
@@ -81,38 +69,28 @@ addBookToLibrary("Game of Thrones", "George RR Martin", "2000", "Read");
 addBookToLibrary("Lord of the Rings", "JRR Tolkien", "200", "Read");
 addBookToLibrary("Skulduggery Pleasant", "Derek Landy", "300", "Unread");
 
-
 function populatePage() {
-	booksContainer.innerHTML = ""
+	booksContainer.innerHTML = "";
 	for (let i = 0; i < library.length; i++) {
-	const bookCard = document.createElement("div");
-	bookCard.setAttribute("class", "book-card");
-		bookCard.innerHTML = 
-	`<h3>${library[i].title}</h3>
+		const bookCard = document.createElement("div");
+		bookCard.setAttribute("class", "book-card");
+		bookCard.innerHTML = `<h3>${library[i].title}</h3>
 	<p>Title: ${library[i].title}</P>
 	<p>Pages: ${library[i].pages}</p>
 	<button class="toggle-read">Read</button>
 	<button class="remove-button">Remove book</button>`;
-	booksContainer.appendChild(bookCard);
+		booksContainer.appendChild(bookCard);
 	}
 }
 
-populatePage()
-
+populatePage();
 
 submitBookButton.addEventListener("click", () => {
-	addBookToLibrary(title.value, author.value, pages.value, getStatus(readingStatus))
-	populatePage()
-})
-
-
-
-
-
-
-
-
-
-
-
-
+	addBookToLibrary(
+		title.value,
+		author.value,
+		pages.value,
+		getStatus(readingStatus)
+	);
+	populatePage();
+});
